df38b2549eecdbc79b1181221508c5848834c506
Sidhant Yadav <83953899+yadavsidhant@users.noreply.github.com>
2024-07-31 19:36:45 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="quickllm",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[
         "transformers>=4.0.0",
