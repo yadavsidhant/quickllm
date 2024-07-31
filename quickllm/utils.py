09bcf5c7b0909b64052e2a3d8431c323e12cf5da
Sidhant Yadav <83953899+yadavsidhant@users.noreply.github.com>
2024-07-31 19:55:13 +0530

    Update utils.py

--- quickllm/utils.py
+++ quickllm/utils.py
@@ -1,11 +1,28 @@
-import pandas as pd
 from datasets import Dataset
 
-def load_dataset(input_file):
-    # Assume the input file is a CSV with 'text' column
-    df = pd.read_csv(input_file)
-    dataset = Dataset.from_pandas(df)
-    return dataset.train_test_split(test_size=0.1)
+def load_dataset(input_file, train_split=0.8, validation_split=None):
+    with open(input_file, 'r', encoding='utf-8') as f:
+        texts = f.readlines()
+    
+    dataset = Dataset.from_dict({"text": texts})
+    
+    if validation_split is None:
+        # Split into train and test (which will be used as validation)
+        splits = dataset.train_test_split(train_size=train_split)
+        return {
+            'train': splits['train'],
+            'validation': splits['test']
+        }
+    else:
+        # Split into train, validation, and test
+        test_split = 1 - train_split - validation_split
+        splits = dataset.train_test_split(train_size=train_split, test_size=(validation_split + test_split))
+        test_valid_split = splits['test'].train_test_split(train_size=validation_split / (validation_split + test_split))
+        return {
+            'train': splits['train'],
+            'validation': test_valid_split['train'],
+            'test': test_valid_split['test']
+        }
 
 def tokenize_dataset(dataset, tokenizer, objective):
     def tokenize_function(examples):
