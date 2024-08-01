c9d0e0779ed1e6d4eba055a135ea8a5cf70b5de6
Sidhant Yadav <83953899+yadavsidhant@users.noreply.github.com>
2024-08-01 20:15:02 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="quickllm",
-    version="0.1.5",
+    version="0.1.6",
     packages=find_packages(),
     install_requires=[
         "transformers>=4.0.0",
@@ -11,6 +11,7 @@ setup(
         "matplotlib>=3.0.0",
         "seaborn>=0.11.0",
         "datasets>=1.0.0",
+        "colorama",
     ],
     author="Sidhant Yadav",
     author_email="supersidhant10@gmail.com",
